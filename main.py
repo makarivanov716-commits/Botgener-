cb18668710d60417814e4733e653ee48885e4ba3
makarivanov716-commits <makarivanov716@gmail.com>
2026-04-17 16:25:21 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+import os
+
+TOKEN = os.getenv("BOT_TOKEN")
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message()
+async def echo(message: types.Message):
+    await message.answer("Бот работает 🚀")
+
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
