6915cb5cc85fd68ae41d46d55546777c73e1f110
makarivanov716-commits <makarivanov716@gmail.com>
2026-04-17 16:45:35 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
 import os
 
 TOKEN = os.getenv("BOT_TOKEN")
@@ -7,9 +8,13 @@ TOKEN = os.getenv("BOT_TOKEN")
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+@dp.message(CommandStart())
+async def start(message: types.Message):
+    await message.answer("Бот работает 🚀")
+
 @dp.message()
 async def echo(message: types.Message):
-    await message.answer("Бот работает 🚀")
+    await message.answer(message.text)
 
 async def main():
     await dp.start_polling(bot)
